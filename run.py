ea71160b0aa217f096764890240038c1b2792789
Pawan Kumar Senapati <51953796+PawanSenapati@users.noreply.github.com>
2023-05-15 23:23:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -22,4 +22,4 @@ app.layout = create_appshell(dash.page_registry.values())
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8080, debug=True)
